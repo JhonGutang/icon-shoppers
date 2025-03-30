9dfc1e377ec2522590da3462e60ae64335fbcb3b
Ian Caballero <ianfredcaballero@gmail.com>
2025-03-30 23:51:33 +0800

    separates the backend order controller and request

---
+++ backend/app/Http/Requests/OrderReqeust.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'quantity' => 'integer|min:1',
+            'total_amount' => 'numeric|min:0',
+            'location' => 'string|max:255',
+            'status' => 'in:cart,ordered,approved,rejected,to_be_delivered,recieved,not_recieved,completed',
+        ];
+    }
+}
