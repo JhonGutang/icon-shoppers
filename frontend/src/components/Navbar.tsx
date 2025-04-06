74001f0b7891ba424b7519a076ea850fe84226ee
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-04-06 18:51:25 +0800

    improved order section for customer

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,12 +16,12 @@ const Navbar:React.FC<NavbarProps> = ({activeComponent, setActiveComponent}) => 
     ];
 
     return ( 
-        <div className="w-[60vw] h-[60px] border-2 border-black rounded-full items-center justify-center flex gap-3 py-3 mb-3">
+        <div className="lg:w-[60vw] lg:min-h-[60px] overflow-x-auto overflow-y-hidden min-h-[6vh] w-[90%] p-2  border-2 border-black rounded-full items-center lg:justify-center flex gap-3 lg:py-3 mb-3">
             {labels.map((label, index) => (
                 <Button 
                     variant='ghost' 
                     key={index} 
-                    className={`hover:bg-green-500 hover:px-10  rounded-full hover:text-white ${activeComponent === label ? 'bg-green-500 text-white px-10' : ''}`}
+                    className={`hover:bg-green-500 lg:hover:px-10  rounded-full hover:text-white ${activeComponent === label ? 'bg-green-500 text-white lg:px-10' : ''}`}
                     onClick={() => setActiveComponent(label)}
                 >
                     {label}
--- frontend/src/components/customer-home/Orders.tsx
+++ frontend/src/components/customer-home/Orders.tsx
@@ -27,57 +27,96 @@ const Orders = () => {
   }, []);
   return (
     <div className="w-full">
-
       <div className="flex justify-between px-10 mb-4">
-        <div className="flex gap-2">
-          <ShoppingCart />
-          <div>Cart</div>
-        </div>
+        <div className="text-xl font-semibold">Orders</div>
         <div>
           <Button>All</Button>
         </div>
       </div>
 
       <div className="px-10">
-        <Card className="py-0">
-          <CardContent className="py-0">
-            <Accordion type="single" collapsible>
-              {orders?.map((order, index) => (
-                <div key={index}>
-                  <AccordionItem value={String(index)}>
-                    <AccordionTrigger className="capitalize">
-                      <div>
-                        {" "}
-                        {order.shop.name} -{" "}
-                        {order.status === "approved"
-                          ? "Processing"
-                          : order.status}{" "}
-                      </div>
-                      <div>Total: ₱{order.total_amount}</div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      {order.products.map((product) => (
-                        <div key={product.id} className="flex gap-2 mb-2">
-                          <img
-                            src={`http://localhost:8000/storage/${product.image}`}
-                            alt=""
-                            className="w-[20vw] rounded-lg"
-                          />
+        {orders?.map((order, index) => (
+          <div key={index} className="mb-5">
+            <div className="mb-1">{order.shop.name}</div>
+            <div className="w-full">
+              <Card>
+                <CardContent className="flex flex-col lg:flex-row justify-between lg:items-center ">
+                  <div className="lg:w-1/2 flex flex-col mb-7 lg:mb-0">
+                    {order.products.map((product) => (
+                      <div key={product.id} className="mb-2 w-full">
+                        <div className="flex  items-center gap-2">
                           <div>
-                            <div>
-                              {product.name} - {product.quantity}x
-                            </div>
-                            <div>Total: ₱{product.price}</div>
+                            <img
+                              src={`http://192.168.1.6:8000/storage/${product.image}`}
+                              alt=""
+                              className="w-[80px]"
+                            />
+                          </div>
+                          <div>
+                            <div>{product.name}</div>
+                            <div>Quantity: {product.quantity}</div>
                           </div>
                         </div>
-                      ))}
-                    </AccordionContent>
-                  </AccordionItem>
-                </div>
-              ))}
-            </Accordion>
-          </CardContent>
-        </Card>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="w-full">
+                    <div className="flex justify-between">
+                      <div className="mb-2 text-lg font-semibold">
+                        Order Summary
+                      </div>
+                      <div className="capitalize">{order.status}</div>
+                    </div>
+                    <div className="flex lg:flex-row flex-col gap-10">
+                      <div className="px-3">
+                        {order.products.map((product, index) => (
+                          <div key={index} className="mb-2">
+                            <div className="flex gap-3">
+                              <div>{product.name}</div>
+                              <div>{product.quantity}x</div>
+                            </div>
+                            <div>
+                              P
+                              {(
+                                Number(product.price) * Number(product.quantity)
+                              ).toFixed(2)}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                      <div>
+                        <div className="flex lg:flex-row flex-col lg:gap-10 mb-5 lg:mb-0">
+                          <div>Address:</div>
+                          <div className="text-end">Cash On Delivery</div>
+                        </div>
+                        <div className="flex lg:flex-row flex-col lg:gap-10 mb-5 lg:mb-0">
+                          <div>Payment Method:</div>
+                          <div className="text-end">Cash On Delivery</div>
+                        </div>
+                        <div className="flex lg:flex-row flex-col lg:gap-10">
+                          <div>Total Amount</div>
+                          <div className="text-end">
+                            Total: P
+                            {order.products
+                              .reduce(
+                                (total, product) =>
+                                  total +
+                                
+                                  Number(product.price) *
+                                    Number(product.quantity),
+                                0
+                              )
+                              .toFixed(2)}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
