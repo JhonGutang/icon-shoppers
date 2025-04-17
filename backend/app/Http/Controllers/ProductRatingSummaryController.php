c8fdd55203d29e96ce1f07b3f9a8ed70e333a5eb
Ian Caballero <ianfredcaballero@gmail.com>
2025-04-18 00:53:25 +0800

    added new rating summary schema and also made rating controllers

--- backend/app/Http/Controllers/ProductRatingSummaryController.php
+++ backend/app/Http/Controllers/ProductRatingSummaryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ShopRatingController extends Controller
+class ProductRatingSummaryController extends Controller
 {
     //
 }
